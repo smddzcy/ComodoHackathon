0ee7e1ac8f35a78564871a61cdc617a67b4eab93
smddzcy <samedduzcay@gmail.com>
2016-04-02 20:41:29 +0300

    ıvır zıvır

--- Config.php
+++ Config.php
@@ -2,6 +2,13 @@
 
 require_once('vendor/autoload.php');
 
+use NlpTools\Tokenizers\WhitespaceTokenizer;
+use NlpTools\Models\FeatureBasedNB;
+use NlpTools\Documents\TrainingSet;
+use NlpTools\Documents\TokensDocument;
+use NlpTools\FeatureFactories\DataAsFeatures;
+use NlpTools\Classifiers\MultinomialNBClassifier;
+
 final class Config
 {
     const MAIL_TYPES = [
--- MailCategorizer.php
+++ MailCategorizer.php
@@ -1,12 +1,5 @@
 <?php
 
-use NlpTools\Tokenizers\WhitespaceTokenizer;
-use NlpTools\Models\FeatureBasedNB;
-use NlpTools\Documents\TrainingSet;
-use NlpTools\Documents\TokensDocument;
-use NlpTools\FeatureFactories\DataAsFeatures;
-use NlpTools\Classifiers\MultinomialNBClassifier;
-
 class MailCategorizer
 {
 
