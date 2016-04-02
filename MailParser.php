0174c7c98818d9bbc20add34a73716530297328d
smddzcy <samedduzcay@gmail.com>
2016-04-02 19:56:53 +0300

    Machine learning framework'ü kuruldu, composer gerekiyor

---
+++ MailParser.php
@@ -0,0 +1,13 @@
+<?php
+
+// Parses mail into keywords and frequencies
+use NlpTools\Tokenizers\WhitespaceTokenizer;
+use NlpTools\Models\FeatureBasedNB;
+use NlpTools\Documents\TrainingSet;
+use NlpTools\Documents\TokensDocument;
+use NlpTools\FeatureFactories\DataAsFeatures;
+use NlpTools\Classifiers\MultinomialNBClassifier;
+class MailParser
+{
+
+}
